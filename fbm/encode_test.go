844e73780c57e59360aab7f899f96784fc2e589f
permito <penciller94@gmail.com>
2024-03-11 15:02:57 +0100

    perlin benchmark

--- fbm/encode_test.go
+++ fbm/encode_test.go
@@ -1,6 +1,8 @@
 package fbm
 
 import (
+	"github.com/Depermitto/noise/noise"
+	"github.com/Depermitto/noise/noise/perlin"
 	"github.com/Depermitto/noise/noise/worley"
 	"image"
 	"testing"
@@ -21,5 +23,14 @@ func BenchmarkWorley1024(b *testing.B) {
 	bounds := image.Rect(0, 0, 1024, 1024)
 	for i := 0; i < b.N; i++ {
 		mod.EncodeImage(wor, bounds, "")
+	}
+}
+
+func BenchmarkPerlin512(b *testing.B) {
+	per := perlin.Make(noise.Linear)
+	mod := New()
+	bounds := image.Rect(0, 0, 512, 512)
+	for i := 0; i < b.N; i++ {
+		mod.EncodeImage(per, bounds, "")
 	}
 }
--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Depermitto/noise/fbm"
+	"github.com/Depermitto/noise/noise"
 	"github.com/Depermitto/noise/noise/chaos"
-	"github.com/Depermitto/noise/noise/worley"
+	"github.com/Depermitto/noise/noise/perlin"
 	"image"
 )
 
-const side = 2 << 8
+const side = 2 << 9
 
 var bounds = image.Rect(0, 0, side, side)
 
 func main() {
 	chaos.SetSeed(6)
-	mod := fbm.New(fbm.WithFreq(0.005))
-	wor := worley.Make(false)
+	mod := fbm.New(
+		fbm.WithFreq(0.003),
+		fbm.WithOctaves(3),
+		fbm.WithLacunarity(3.0),
+	)
+	//wor := worley.Make(true)
+	per := perlin.Make(noise.Linear)
 
-	mod.EncodeImage(wor, bounds, "worley.png")
-
-	for k, v := range wor.Cells {
-		fmt.Println(k, v)
-	}
+	//mod.EncodeImage(wor, bounds, "worley.png")
+	//fmt.Println(len(wor.Cells))
+	mod.EncodeImage(per, bounds, "perlin.png")
 }
